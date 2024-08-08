efc55659a983a3d29c1446552f4b1f25eb71b321
Will Morris <morris.william.work@gmail.com>
2024-08-08 16:39:33 -0400

    Switch robust_visitor to use proper set, remove explicit IO

--- src/robust_vistor.rb
+++ src/robust_vistor.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_relative "fault_model"
 
 # RobustVisitor.
 # Traverse a lattice of faults, finding the maximal combinations of faults that are operational.
@@ -9,32 +10,34 @@
 # Author: William Morris
 
 class RobustVisitor
+  attr_reader :robustness
+
   # The RobustVisitor takes a path to a TLA+ model.
   # This model will be modified by the visitor.
+  def initialize(model_path, config_path)
+    @model_path = model_path
+    @config_path = config_path
 
-  # Assumption: the modelpath contains no spec.
-  def initialize(modelpath)
-    @path = modelpath
-    @robustness = {}
-  end
-
-  # ***** IO OPERATIONS ***** #
-
-  def open
-    @file = File.open(@path, "rw")
-  end
-
-  def close
-    @file.close
+    @model = FaultModel.new @model_path
+    @robustness = Set.new
   end
 
   # ***** LATTICE TRAVERSAL ***** #
 
   # Visit: currently, we assume that the traversal process will be serialized.
-  # If not, we'll probably want to find a better language for parallelization
-  # Bc we're going to need shared state to track the robust sets.
   def visit(faults)
-    # Add to specification a single line with the set of faults.
-    true
+    # Make sure we're checking against this set of faults!
+    @model.fault_spec(faults)
+
+    # Model check
+    if system "tlc", @model_path, "-config", @config_path
+      # If we're robust against this combination of faults, add it as a maximal robust envelope.
+      # Do not traverse further.
+      @robustness << faults
+      true
+    else
+      # Otherwise, descend to subsets.
+      false
+    end
   end
 end
