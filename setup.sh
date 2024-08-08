d2f9d6747ba61d548c945358c91da1e488491710
Will Morris <morris.william.work@gmail.com>
2024-08-08 14:57:20 -0400

    Robustify (ha) setup script; ensure run in correct dir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
+if [ ! -f faultcrosser.rb ]
+then
+  echo "faultcrosser.rb not found! Are you in faultcrosser root directory?"
+  exit 1
+fi
+
+if [ ! -d src ]
+then
+  echo "src directory not found! Are you in faultcrosser root directory?"
+  exit 1
+fi
+
 mkdir -p examples/twophase && cd examples/twophase || exit
 
 ln -s ../../faultcrosser.rb .
